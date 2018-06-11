c836c310e267e3a9879159434762d1b2bdc35196
Jefwillems <jefwillems100@gmail.com>
2018-06-11 11:16:25 +0200

    add scss support

---
+++ src/components/Quiz.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="quiz">
+
+    </div>
+</template>
+<script>
+import { mapState, mapGetters } from 'vuex';
+import types from '../store/types';
+
+export default {
+  computed: {
+    ...mapState([]),
+    ...mapGetters(['alphabeticalCategories', 'cat_question_count']),
+  },
+  methods: {
+    getCategories() {
+      this.$store.dispatch(types.actions.FETCH_CATEGORIES);
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+</style>

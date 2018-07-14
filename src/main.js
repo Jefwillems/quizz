48618fdda42df05d4006800cd0f3198f99ae5d25
Jefwillems <jefwillems100@gmail.com>
2018-07-14 22:39:04 +0200

    add vue-select and logger

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueSelect from 'vue-select';
 import App from './App';
 import router from './router';
 import store from './store';
+import logger, { options } from './logger';
 
 Vue.config.productionTip = false;
+Vue.use(logger, options);
+Vue.component('v-select', VueSelect);
 
 /* eslint-disable no-new */
 new Vue({

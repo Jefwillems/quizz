5686a18d177862c1acf8ff38287b44236a69a68a
Jefwillems <jefwillems100@gmail.com>
2019-01-10 11:39:55 +0100

    enable tree-shaking

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -2,7 +2,7 @@
   v-card(dark color="secondary")
     v-card-text(class="px-0 font-weight-bold headline" v-html="selected_question.question")
     v-card-text(class="px-0 pb-0 pt-4")
-      span(class="") {{questions_length}} questions remaining after this
+      span {{questions_length}} questions remaining after this
     v-card-text(class="px-0 py-0")
       v-btn(flat color="orange" @click="fetchMoreQuestions") get more
 </template>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin');
+
 module.exports = {
   outputDir: './docs',
   baseUrl: process.env.NODE_ENV === 'production' ? './' : '/',
+  configureWebpack: {
+    plugins: [new VuetifyLoaderPlugin()],
+  },
 };

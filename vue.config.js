2be559bf84ead5b74a08f76bb1d3ab8c60da5361
Jefwillems <jefwillems100@gmail.com>
2019-01-22 09:48:56 +0100

    update docs

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin');
 
 module.exports = {
   outputDir: './docs',
-  baseUrl: process.env.NODE_ENV === 'production' ? './' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
   configureWebpack: {
     plugins: [new VuetifyLoaderPlugin()],
   },
